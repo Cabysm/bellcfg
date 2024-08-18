e12176bb4534f3a2c04ed09db5a27bc2a2574c84
Erabysm <12790921+erabysm@user.noreply.gitee.com>
2024-08-18 18:09:25 +0800

    v0.1.1

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ fn main() -> Result<(), PlatformError> {
     let app_handler = app.as_weak().unwrap();
     app.on_generate(move || {
         let raw = app_handler.get_inputs();
-        let mut str = SharedString::from("Made by space.bilibili.com/9832924 \n\nbind n +next\nalias +next \"ang0;+attack;-attack;\"\nalias -next n0\n");
+        let mut str = SharedString::from("//Made by space.bilibili.com/9832924 \n\nbind n +next\nalias +next \"ang0;+attack;-attack;\"\nalias -next n0\n");
         let raw: Vec<&str> = raw.split_whitespace().collect();
         let len=raw.len();
         for i in 1..len{
